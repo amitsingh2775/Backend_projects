e994541a77adb6e2958ddc3ccbd630396cb1fa02
amit kumar <amitsinghbadram@gmail.com>
2024-07-03 16:51:40 +0530

    cloudinary setup and multer middleware

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,3 +7,26 @@ cloudinary.config({
     api_key:  process.env.API_KEY, 
     api_secret: process.env.API_SECRET
 });
+
+const upoloadOnCloudnariy=async(localPath)=>{
+   try {
+
+    if(!localPath) return null
+
+    // upload file on cloudinary
+  const res= await cloudinary.uploader.upload(localPath,{
+        resource_type:"auto"
+    })
+    // file uploaded succesfully
+    console.log("file uploaded",res.url);
+    return res;
+    
+   } catch (error) {
+    // yani server pe too wo file aa chuki thhi ab server se hata do yani delete kar do sync way me
+    // remove localy saved temp file 
+     fs.unlinkSync(localPath)
+     return null
+   }
+}
+
+export {upoloadOnCloudnariy}
